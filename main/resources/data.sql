a18edc046842244b22f1f8799ace5485579af1d6
Asafe Silva <asafesilva13@gmail.com>
2025-09-18 17:18:56 -0300

    Create data.sql

---
+++ main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, full_name, email, created_at) VALUES
+('maria', 'Maria Silva', 'maria@pmup.dev', CURRENT_TIMESTAMP),
+('joao',  'João Souza', 'joao@pmup.dev',  CURRENT_TIMESTAMP);
